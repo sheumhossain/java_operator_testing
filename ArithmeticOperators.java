588b51a6e1b8993bc65637a18f37701c22084d9f
sheum <37168380+sheumhossain@users.noreply.github.com>
2019-01-07 19:19:25 +0600

    Add files via upload

---
+++ ArithmeticOperators.java
@@ -0,0 +1,19 @@
+package main;
+
+public class ArithmeticOperators {
+	public int add(int x,int y) {
+		return x+y;
+	}
+	public int sub(int x,int y) {
+		return x-y;
+	}
+	public int mul(int x,int y) {
+		return x*y;
+	}
+	public int div(int x,int y) {
+		return x/y;
+	}
+	public int mod(int x,int y) {
+		return x%y;
+	}
+}
